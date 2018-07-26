ef5f8fcc2226357d7112bf0517d4e4828cff9bf2
John Carlisle <jmcarlisle@live.com>
2018-07-26 15:52:08 -0700

    completed class demo

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import {Person, SuperPerson} from "./person";
+
+
+const bob = new Person('Bob');
+const superSarah = new SuperPerson('Sarah', "speed");
+
+console.log(bob.speak("my name is bob"));
+
+superSarah.speak('Hi im sarah');
+
+superSarah.saySuperPower();
---
+++ app/person.js
@@ -0,0 +1,24 @@
+import $ from 'jquery';
+
+export class Person {
+    constructor(name){
+        this.name=name;
+    }
+
+    speak( text ) {
+        return ` ${this.name} says: ${text}`;
+    }
+}
+
+export class SuperPerson extends Person {
+    constructor(name, superpower ){
+        super(name);
+        this.superpower = superpower;
+    }
+    speak(text){
+        $('body').append(`<h1>${super.speak(text)}</h1>`);
+    }
+    saySuperPower(){
+        this.speak(` my super power is ${this.superpower}`);
+    }
+}
